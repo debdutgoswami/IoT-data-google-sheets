79e5d70937445601c4fa0869c37a879d4b85ea50
Debdut Goswami <debdutgoswami@gmail.com>
2019-12-25 16:54:43 +0530

    Add files via upload

---
+++ sending_data.ino
@@ -0,0 +1,74 @@
+#include <Arduino.h>
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+
+#include <ESP8266HTTPClient.h>
+
+#include <WiFiClient.h>
+
+const char* ssid = "ssid";
+const char* password = "password";
+
+ESP8266WiFiMulti WiFiMulti;
+String mainURL ="your url for API call?";
+
+void setup() {
+  Serial.begin(9600);
+
+  Serial.println();
+  Serial.println();
+  Serial.println();
+
+  for (uint8_t t = 4; t > 0; t--) {
+    Serial.printf("[SETUP] WAIT %d...", t);
+    Serial.flush();
+    delay(1000);
+  }
+
+  WiFi.mode(WIFI_STA);
+  WiFiMulti.addAP(ssid, password);
+}
+
+void loop() {
+  // wait for WiFi connection
+  if ((WiFiMulti.run() == WL_CONNECTED)) {
+
+    WiFiClient client;
+
+    HTTPClient http;
+
+    float sensorValue = analogRead(A0);
+    
+    Serial.print("[HTTP] begin...");
+    delay(100);
+    if (http.begin(client, mainURL+"temp="+String(sensorValue))) {  // HTTP
+
+      Serial.println(mainURL+"temp="+String(sensorValue));
+      Serial.print("[HTTP] GET...");
+      // start connection and send HTTP header
+      int httpCode = http.GET();
+
+      // httpCode will be negative on error
+      if (httpCode > 0) {
+        // HTTP header has been send and Server response header has been handled
+        Serial.printf("[HTTP] GET... code: %d", httpCode);
+
+        // file found at server
+        if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+          String payload = http.getString();
+          Serial.println(payload);
+        }
+      } else {
+        Serial.printf("[HTTP] GET... failed, error: %s", http.errorToString(httpCode).c_str());
+      }
+
+      http.end();
+    } else {
+      Serial.printf("[HTTP} Unable to connect");
+    }
+  }
+
+  delay(10000);
+}
+   
